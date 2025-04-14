9b8445470d3495a11571a9011701ad9abd79c856
ahmed yasser <ahmedya3ser8.com>
2025-04-14 16:29:00 +0200

    create product-details logic

--- src/app/pages/product-details/components/details/details.component.ts
+++ src/app/pages/product-details/components/details/details.component.ts
@@ -1,7 +1,12 @@
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, PLATFORM_ID, signal, WritableSignal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../../../core/services/products/products.service';
 import { IProduct } from '../../../../shared/interfaces/iproduct';
+import { CartService } from '../../../../core/services/cart/cart.service';
+import { Subject, takeUntil } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { WishlistService } from '../../../../core/services/wishlist/wishlist.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -9,10 +14,12 @@ import { IProduct } from '../../../../shared/interfaces/iproduct';
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   imageCover: string = "https://ecommerce.routemisr.com/Route-Academy-products/1680403397402-cover.jpeg";
   colorName: string = 'Blue';
   sizeName: string = 'Medium';
+  productIds: string[] = [];
+  destory$: Subject<any> = new Subject();
   colors = [
     {name: 'Blue', color: '#507ccd'},
     {name: 'White', color: '#fff'},
@@ -33,8 +40,18 @@ export class DetailsComponent implements OnInit {
   productItem: WritableSignal<IProduct> = signal({} as IProduct);
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly productsService = inject(ProductsService);
+  private readonly cartService = inject(CartService);
+  private readonly wishlistService = inject(WishlistService);
+  private readonly toastrService = inject(ToastrService);
+  private readonly pLATFORM_ID = inject(PLATFORM_ID);
   ngOnInit(): void {
     this.getSpecificProduct();
+    if (isPlatformBrowser(this.pLATFORM_ID)) {
+      const ids = localStorage.getItem("productIds");
+      if(ids) {
+        this.productIds = JSON.parse(ids);
+      }
+    }
   }
   getSpecificProduct(): void {
     this.activatedRoute.paramMap.subscribe({
@@ -56,5 +73,42 @@ export class DetailsComponent implements OnInit {
   }
   selectedSize(name: string): void {
     this.sizeName = name;
+  }
+  addToCart(productId: string): void {
+    this.cartService.addProductToCart(productId).pipe(takeUntil(this.destory$)).subscribe({
+      next: (res) => {
+        if (res.status === "success") {
+          this.cartService.cartNumber.set(res.numOfCartItems);
+          this.toastrService.success(res.message);
+        }
+      }
+    })
+  }
+  addProductToWishlist(productId: string): void {
+    this.wishlistService.addProductToWishlist(productId).pipe(takeUntil(this.destory$)).subscribe({
+      next: (res) => {
+        if (res.status === 'success') {
+          this.wishlistService.wishlistNumber.set(res.data.length);
+          this.productIds = res.data || [];
+          localStorage.setItem('productIds', JSON.stringify(this.productIds));
+          this.toastrService.success(res.message);
+        }
+      }
+    })
+  }
+  removeProductFromWishlist(productId: string): void {
+    this.wishlistService.removeProductFromWishlist(productId).pipe(takeUntil(this.destory$)).subscribe({
+      next: (res) => {
+        if (res.status === "success") {
+          this.wishlistService.wishlistNumber.set(res.data.length);
+          this.productIds = res.data || [];
+          localStorage.setItem('productIds', JSON.stringify(this.productIds));
+          this.toastrService.success(res.message);
+        }
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    this.destory$.next("Done");
   }
 }
